5229dfc0e4089b2cf82ed0b82ee971eae8bf4aab
Vijay Patidar <vramotiya@gmail.com>
2021-03-07 22:33:04 +0530

    logout feature added

--- gateway-service/src/main/java/com/svceindore/gatewayservice/controllers/AuthController.java
+++ gateway-service/src/main/java/com/svceindore/gatewayservice/controllers/AuthController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.WebSession;
+import reactor.netty.http.server.HttpServerRequest;
 
 import java.net.URI;
 
@@ -23,11 +25,18 @@ public class AuthController {
     @Value("${PROJECT_HOST_ADDRESS}:8080")
     private String PROJECT_ROOT;
 
+    @Value("${PROJECT_HOST_ADDRESS}:8180")
+    private String PROJECT_AUTH_SERVER_ROOT;
+
     @Bean
     RouterFunction<ServerResponse> routerFunction() {
         return route(GET("/auth/login"), req ->
                 ServerResponse.temporaryRedirect(URI.create(PROJECT_ROOT + "/home"))
                         .build());
+    }
+    @Bean
+    RouterFunction<ServerResponse> routerFunctionLogout() {
+        return route(GET("/auth/logout"), req -> ServerResponse.temporaryRedirect(URI.create(PROJECT_AUTH_SERVER_ROOT + "/auth/realms/CollageERP/protocol/openid-connect/logout?redirect_uri="+PROJECT_ROOT)).build());
     }
 
 }
