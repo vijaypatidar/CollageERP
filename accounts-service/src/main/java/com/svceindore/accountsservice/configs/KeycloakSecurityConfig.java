ff7b9ced4977e5e37fa7ce049df1640402f8f01c
Vijay Patidar <41512842+vijaypatidar@users.noreply.github.com>
2021-05-23 20:46:43 +0530

    accounts service created

---
+++ accounts-service/src/main/java/com/svceindore/accountsservice/configs/KeycloakSecurityConfig.java
@@ -0,0 +1,65 @@
+package com.svceindore.accountsservice.configs;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    public final KeycloakClientRequestFactory keycloakClientRequestFactory;
+
+    public KeycloakSecurityConfig(KeycloakClientRequestFactory keycloakClientRequestFactory) {
+        this.keycloakClientRequestFactory = keycloakClientRequestFactory;
+    }
+
+    @LoadBalanced
+    @Bean
+    public KeycloakRestTemplate keycloakRestTemplate() {
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.cors().and().authorizeRequests()
+                .antMatchers("/api/**")
+                .authenticated()
+                .anyRequest().authenticated();
+        http.csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
+        //simpleAuthorityMapper.setConvertToUpperCase(true);
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
---
+++ accounts-service/src/main/java/com/svceindore/accountsservice/configs/Roles.java
@@ -0,0 +1,9 @@
+package com.svceindore.accountsservice.configs;
+
+public class Roles {
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_STUDENT = "ROLE_STUDENT";
+    public static final String ROLE_FACULTY = "ROLE_FACULTY";
+    public static final String ROLE_LIBRARIAN = "ROLE_LIBRARIAN";
+    public static final String ROLE_ACCOUNTANT = "ROLE_ACCOUNTANT";
+}
